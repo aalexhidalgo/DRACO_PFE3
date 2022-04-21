bba4a996c3451efc8c9529c97319fcce671a171e
aalexhidalgo <91455208+aalexhidalgo@users.noreply.github.com>
2022-04-21 13:43:13 +0200

    21/04/2022

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Variables
+    private AudioSource GameManagerAudioSource;
+    private AudioSource MainCameraAudioSource;
+    //Paneles
+
+    public GameObject MainMenuPanel;
+    public GameObject OptionsPanel;
+    public GameObject HowToPlayPanel;
+    public GameObject PauseMenuPanel;
+
+    //Panel principal
+
+    public void StartButton()
     {
-        
+        Debug.Log("Start");
+        SceneManager.LoadScene("Game");
+    }
+    public void HowToPlayButton()
+    {
+        MainMenuPanel.SetActive(false);
+        HowToPlayPanel.SetActive(true);
+    }
+    public void OptionsButton()
+    {
+        MainMenuPanel.SetActive(false);
+        OptionsPanel.SetActive(true);
+    }
+    public void ExitButton()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
     }
 
-    // Update is called once per frame
-    void Update()
+    //Volver hacia atrás
+
+    public void ReturnButton()
     {
-        
+        OptionsPanel.SetActive(false);
+        HowToPlayPanel.SetActive(false);
+        MainMenuPanel.SetActive(true);
     }
+
+    //OptionsPanel
+
+    //PauseMenuPanel
+    /*
+    public void RestartButton()
+    {
+        ReloadScene
+    }
+
+    public void ResumeButton()
+    {
+        Desactivar panel de pausa y volver al juego
+    }
+
+    */
+
+    public void PauseMenuButton()
+    {
+        PauseMenuPanel.SetActive(true);
+    }
+
+    void Start()
+    {
+        MainMenuPanel.SetActive(true);
+        OptionsPanel.SetActive(false);
+        HowToPlayPanel.SetActive(false);
+
+        //Audiosource
+        GameManagerAudioSource = GetComponent<AudioSource>();
+        MainCameraAudioSource = GameObject.Find("Main Camera").GetComponent<AudioSource>();
+    }
+
 }
